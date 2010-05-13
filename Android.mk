d0b71b7cd529902ac1e1cd27c285f3708ea11054
cvpcs <root@cvpcs.org>
2010-05-13 07:39:06 -0500

    Added an android makefile to the build

---
+++ Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:=\
+	database.c do_command.c \
+	entry.c	env.c job.c misc.c \
+	popen.c pw_dup.c user.c cron.c
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE := cron
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:=\
+	misc.c entry.c env.c \
+	pw_dup.c crontab.c
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE := crontab
+include $(BUILD_EXECUTABLE)
